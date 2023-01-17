9944092f55e79545fe9c938d7577046208ccbc06
AtamMN <mkhatamramadhan@gmail.com>
2023-01-17 19:05:54 +0700

    fix navbar #2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Navbar(){
-    const [nav, setNav] = useState(true);
+    const [nav, setNav] = useState();
 
     return (
     <div className="flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed">
@@ -19,10 +19,10 @@ function Navbar(){
 
         
         <div onClick={() => setNav(!nav)} className="cursor-pointer z-10 text-gray-300 md:hidden">
-            {nav ? <FaBars size={30}/> : <FaTimes size={30}/>}
+            {!nav ? <FaBars size={30}/> : <FaTimes size={30}/>}
         </div>
 
-        {!nav && <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black/[0.95] to-gray-700/[0.95] gap-6">
+        {nav && <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black/[0.95] to-gray-700/[0.95] gap-6 md:hidden">
             <li>ABOUT</li>
             <li>OUR WORKS</li>
             <li>OUR TEAM</li>
