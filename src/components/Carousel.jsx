661fa0921fabf5600b7d6ef29e20e1962e0b41f4
AtamMN <mkhatamramadhan@gmail.com>
2023-01-18 11:52:22 +0700

    fix carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaDotCircle } from "react-icons/fa";
+import bg from "../assets/bg.jpg";
+import about from "../assets/about-bg.jpg";
 
 function Carousel(){
     const slides = [
         {
-            url: "https://images.unsplash.com/photo-1589779255235-85dc2a054145?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29ycG9yYXRlJTIwYnVpbGRpbmd8ZW58MHx8MHx8&w=1000&q=80"
+            url: bg
         },
         {
-            url: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        },
+            url: about
+        }
     ];
 
     const [current, setCurrent] = useState(0)
@@ -25,9 +27,13 @@ function Carousel(){
         setCurrent(newSlide)
     }
 
+    const goToSlide = (slideIndex) => {
+        setCurrent(slideIndex)
+    }
+
     return(
         <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 group">
-            <div style={{backgroundImage: `url(${slides[current].url})`}} className="absoulute w-full h-full bg-center bg-cover duration-500">
+            <div style={{backgroundImage: `url(${slides[current].url})`}} className="w-full h-full bg-center bg-cover duration-500">
                 {current === 0 ? 
                     <div className="absolute top-[50%] rounded-2xl -translate-x-0 traslate-y-[-50%] left-32 text-2xl bg-black/60 text-white duration-500">
                         <h2 className="font-bold px-2 py-2">A PLACE WHERE WE CREATE WONDERFUL THINGS</h2>
@@ -46,6 +52,13 @@ function Carousel(){
                 {/* right */}
                 <div className="hidden absolute group-hover:block top-[50%] -translate-x-0 traslate-y-[-50%] right-4 text-2xl bg-black/20 text-white cursor-pointer">
                     <FaArrowRight onClick={nextSlide} size={30}/>
+                </div>
+                <div className="flex gap-2 justify-center pt-4">
+                    {slides.map((slide, slideIndex) => (
+                        <div className="text-white cursor-pointer pt-[20px]" key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+                            <FaDotCircle size={30}/>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
