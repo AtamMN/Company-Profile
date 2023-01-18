a238000fe6e7dfc67c6759d89e515a25b82d225e
AtamMN <mkhatamramadhan@gmail.com>
2023-01-18 13:51:36 +0700

    fixing content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ function Content(){
     return(
         <div className="w-full">
             <h1 className="font-bold text-lg">OUR VALUES</h1>
-            <div className="flex gap-10 justify-center py-5">
+            <div className="flex flex-wrap gap-10 justify-center py-5">
                 <div className="flex flex-col bg-red-400 text-white px-10 py-10 items-center">
                     <FaBrain size={30} className="mb-5"/>
                     <h1 className="font-semibold">INNOVATIVE</h1>
